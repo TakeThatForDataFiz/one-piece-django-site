01c6f5f45251553512807e0ec731c4c4b1d016ad
John Lunz <johnlunz8@gmail.com>
2022-08-02 22:16:25 -0400

    add url config and sample view to news app

--- onepiece/news/views.py
+++ onepiece/news/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# create temp sample view to be replaced
+
+
+def index(request):
+    return HttpResponse("Hello World, you are at the news index")
