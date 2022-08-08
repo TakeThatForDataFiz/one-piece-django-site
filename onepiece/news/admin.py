bdd727476a04a690f9ca58416702071a58b31ebb
John Lunz <johnlunz8@gmail.com>
2022-08-07 22:51:29 -0400

    Registered Models to admin site

--- onepiece/news/admin.py
+++ onepiece/news/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Manga, Anime, Episode, Article, Chapter
 
 # Register your models here.
+admin.site.register(Manga)
+admin.site.register(Anime)
+admin.site.register(Episode)
+admin.site.register(Article)
+admin.site.register(Chapter)
