57fe6effd408471e25ee0cce01a1ccc76eb94461
John Lunz <johnlunz8@gmail.com>
2022-08-18 23:23:29 -0400

    Added Test Cases for Simple Manga Handler Custom Command

--- onepiece/news/management/commands/startjobs.py
+++ onepiece/news/management/commands/startjobs.py
@@ -1,7 +1,23 @@
 from django.core.management.base import BaseCommand
+from dateutil import parser
+from bs4 import BeautifulSoup
+import requests
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        # Function that will add parsing details for API Request
-        print("It Works")
+        # Get manga details via API
+        tags = get_bs4_soup()
+        parse_manga_information(tags)
+
+    def get_bs4_soup(self):
+        res = requests.get(
+            "https://cdn.animenewsnetwork.com/encyclopedia/api.xml?manga=1223")
+        tags = BeautifulSoup(res.content, 'lxml-xml')
+        return tags
+
+    def parse_manga_information(self, tags):
+        # parse manga xml information and return as unpacked tuple
+        name = tags.manga['name']
+        des = tags.find(type='Plot Summary').contents[0]
+        return (name, des)
--- onepiece/news/tests.py
+++ onepiece/news/tests.py
@@ -1,9 +1,37 @@
 from django.test import TestCase
 from django.utils import timezone, dateformat
-from .models import Episode
+from .models import Episode, Manga
 from django.urls.base import reverse
+from .management.commands.startjobs import Command
 
 # Create your tests here.
+
+
+class MangaSimpleHandlerTest(TestCase):
+    ''' Unit Test Class to test Handler for Manga API request'''
+
+    def setUp(self):
+        self.command = Command()
+        self.manga = Manga.objects.create(name="One Piece",
+                                          plot_summary='Twenty two years ago, the legendary pirate, Gold Roger was executed. His final words told')
+        self.tags = self.command.get_bs4_soup()
+        self.parsed_name, self.des = self.command.parse_manga_information(
+            self.tags)
+
+    def test_api_response_parse(self):
+        '''test case that determines whether lxml parsing returned non-empty response'''
+        self.assertTrue(self.tags)
+
+    def test_manga_name_parsed(self):
+        '''test case that determines whether manga name is parsed correctly'''
+        self.assertEqual(self.parsed_name, self.manga.name)
+
+    def test_manga_description_parsed(self):
+        '''test case that determines whether manga description is parsed correctly'''
+        # normalize incoming descriptions
+        norm_des = self.des.lower().replace(' ', '')
+        norm_sum = self.manga.plot_summary.lower().replace(' ', '')
+        self.assertIn(norm_sum, norm_des)
 
 
 class EpiosdeTests(TestCase):
